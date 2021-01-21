267f193b450512c1e80a8249715d38ccaaa1b388
Bert Keller <bertwinkeller@gmail.com>
2021-01-21 11:31:15 -0800

    updated react project

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Home from './component/pages/Home'
+import About from './component/pages/About'
+
 
 const App = () => {
   return (
-    <div className="App">
-      My App
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </div>
+
+    </Router>
   );
 }
 
