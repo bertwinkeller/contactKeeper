e9972d63907b5b0839dafe22e9803d1621727ce5
Bert Keller <bertwinkeller@gmail.com>
2021-01-21 10:32:40 -0800

    new app title

--- routes/contacts.js
+++ routes/contacts.js
@@ -57,15 +57,75 @@ router.post('/', [auth, [
 //@route PUT api/contacts
 //@desc Update a contact
 //@access Private
-router.put('/:id', (req, res) => {
-    res.send('Update Contact')
+router.put('/:id', auth, async (req, res) => {
+
+    const { name, email, phone, type } = req.body
+
+    //Build Object
+
+    const contactFields = {}
+
+    if (name) contactFields.name = name
+    if (email) contactFields.email = email
+    if (phone) contactFields.phone = phone
+    if (type) contactFields.type = type
+
+    try {
+
+
+        let contact = await Contact.findById(req.params.id)
+
+        if (!contact) return res.status(404).json({ msg: "Contact not found" })
+
+        //Make sure user owns contact
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not Authorized' })
+        }
+
+
+        contact = await Contact.findByIdAndUpdate(req.params.id, {
+            $set: contactFields
+        },
+            {
+                new: true
+            })
+
+        res.json(contact)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
+    }
+
 })
 
 //@route DELETE api/contacts
 //@desc Delete a contact
 //@access Private
-router.delete('/:id', (req, res) => {
-    res.send('Delete Contact')
+router.delete('/:id', auth, async (req, res) => {
+    try {
+
+
+        let contact = await Contact.findById(req.params.id)
+
+        if (!contact) return res.status(404).json({ msg: "Contact not found" })
+
+        //Make sure user owns contact
+
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not Authorized' })
+        }
+
+
+        await Contact.findByIdAndRemove(req.params.id)
+
+        res.json({ msg: 'Contact Removed' })
+
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 
