00ba2171ba7565604ce24ce08f1b59b5b1f98b39
Bert Keller <bertwinkeller@gmail.com>
2021-01-21 14:13:54 -0800

    new state and hooks

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/layout/Navbar'
-import Home from './component/pages/Home'
-import About from './component/pages/About'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import ContactState from './context/contact/ContactState'
 
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
+    <ContactState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
 
-    </Router>
+      </Router>
+    </ContactState>
   );
 }
 
